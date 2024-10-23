20a74ce1240f472ea7b167afb9661a294468fb7e
Ian Lavery <ian@picovoice.ai>
2024-10-23 14:28:31 -0700

    package.swfit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "ios-voice-processor",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "ios_voice_processor",
+            targets: ["ios_voice_processor"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ios_voice_processor",
+            dependencies: [],
+            sources: ["VoiceProcessor*.swift"],
+            linkerSettings: [
+                .linkedFramework("AVFoundation")
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
